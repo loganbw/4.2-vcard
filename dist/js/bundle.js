536e5c61ad040005be1d5e012e4cf574c96c22be
Gerald Emory <geraldemory@Geralds-MacBook-Pro.local>
2017-08-14 15:59:23 -0400

    fixed issues

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,42 +1,43 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
-let $ = require('jquery');
-let handlebars = require('handlebars');
-let token = false;//require('./githubToken.js').token;
+'use strict';
+
+var $ = require('jquery');
+var handlebars = require('handlebars');
+var token = false; //require('./githubToken.js').token;
 
 if (token) {
-let options ={
-  headers: {
+  var options = {
+    headers: {
       'Authorization': ' token ' + token
-  }
-};
-}else {
-  let options = {};
+    }
+  };
+} else {
+  var _options = {};
 }
 init();
 
-
-function displayGit(profile){
+function displayGit(profile) {
   var $card = $('#card');
   var source = $('#git-template').html();
   var template = handlebars.compile(source);
-$('.container').html(template(profile));
-console.log(template(profile));
-//     (function(git){
-//     var $vCard = $(template(git));
-//     // console.log($vCard);
-//     $card.append($vCard);
-//   });
- }
+  $('.container').html(template(profile));
+  console.log(template(profile));
+  //     (function(git){
+  //     var $vCard = $(template(git));
+  //     // console.log($vCard);
+  //     $card.append($vCard);
+  //   });
+}
 
- function init(){
-   fetch('https://api.github.com/users/loganbw').then(function(response){
-     return response.json();
-   }).then(function(data){
+function init() {
+  fetch('https://api.github.com/users/loganbw').then(function (response) {
+    return response.json();
+  }).then(function (data) {
     console.log(data);
     displayGit(data);
   });
- }
+}
 
 },{"handlebars":32,"jquery":44}],2:[function(require,module,exports){
 (function (process,__filename){
